b1132d65e6d14ea99e98044c78bfc2ea01bfcca2
unknown <nomad8t@gmail.com>
2025-01-08 23:22:47 +0300

    feat: add user seed

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { hashSync } from "bcrypt";
+
+import { prisma } from "./prisma-client";
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "User Test",
+        email: "user@test.ru",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+      {
+        fullName: "Admin Admin",
+        email: "admin@test.ru",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "ADMIN",
+      },
+    ],
+  });
+}
+
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
