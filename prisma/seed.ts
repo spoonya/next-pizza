69f46a03544c084ac0267f2fc868dff5574d0099
unknown <nomad8t@gmail.com>
2025-01-09 23:48:38 +0300

    fix: add ingredients to cart prisma scheme

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -188,6 +188,32 @@ async function up() {
       generateProductItem({ productId: 17 }),
     ],
   });
+
+  await prisma.cart.createMany({
+    data: [
+      {
+        userId: 1,
+        totalAmount: 0,
+        token: "11111",
+      },
+      {
+        userId: 2,
+        totalAmount: 0,
+        token: "222222",
+      },
+    ],
+  });
+
+  await prisma.cartItem.create({
+    data: {
+      productItemId: 1,
+      cartId: 1,
+      quantity: 2,
+      ingredients: {
+        connect: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+      },
+    },
+  });
 }
 
 async function down() {
