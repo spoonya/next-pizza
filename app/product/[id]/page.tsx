2aab0f0fe7a03764ed4623fb62c88cabd1a20bda
unknown <nomad8t@gmail.com>
2025-01-19 15:04:18 +0300

    wip: product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,9 +1,70 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
-export default function ProductPage({
+import {
+  Container,
+  GroupVariants,
+  ProductImage,
+  Title,
+} from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
+
+export default async function ProductPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <div>Product {id}</div>;
+  const product = await prisma.product.findFirst({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} size={20} />
+
+        <div className="w-[490px] bg-[#f7f7f7] p-7">
+          <Title
+            text={product.name}
+            size="md"
+            className="font-extrabold mb-1"
+          />
+
+          <p className="text-gray-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse
+            beatae molestias magnam architecto excepturi perspiciatis, quam
+            aperiam est quas sunt totam pariatur? Quam commodi amet, consectetur
+            quod exercitationem praesentium sit?
+          </p>
+
+          <GroupVariants
+            items={[
+              {
+                name: "20 см",
+                value: "20",
+                disabled: false,
+              },
+              {
+                name: "30 см",
+                value: "30",
+                disabled: false,
+              },
+              {
+                name: "40 см",
+                value: "40",
+                disabled: false,
+              },
+            ]}
+            value="20"
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
--- components/shared/product-card.tsx
+++ components/shared/product-card.tsx
@@ -24,7 +24,7 @@ export function ProductCard({
 }: ProductCardProps) {
   return (
     <div className={className}>
-      <Link href="/product/1">
+      <Link href={`/product/${id}`}>
         <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
           <img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
         </div>
